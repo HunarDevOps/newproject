6bfaa839a34d8d7982b0818672f819554897944e
Hunar <hunarmehndiratta42@gmail.com>
2025-10-26 15:50:31 +0530

    Implement main application structure with routing

---
+++ client/client/src/App.tsx
@@ -0,0 +1,37 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import Home from "@/pages/Home";
+import Search from "@/pages/Search";
+import HotelDetail from "@/pages/HotelDetail";
+import Booking from "@/pages/Booking";
+import Confirmation from "@/pages/Confirmation";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/search" component={Search} />
+      <Route path="/hotel/:id" component={HotelDetail} />
+      <Route path="/booking/:hotelId/:roomId" component={Booking} />
+      <Route path="/confirmation/:bookingId" component={Confirmation} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Toaster />
+        <Router />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
